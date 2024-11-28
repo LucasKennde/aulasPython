88fd442ca4628f3e200aeae6940cbb4c7b139514
Lucas Kennde <166322461+LucasKennde@users.noreply.github.com>
2024-11-28 11:54:43 -0300

    remove itens

--- aula4/numerosPares.py
+++
@@ -1,7 +0,0 @@
-numerosPares= 0
-for numero in range(1,51):
-    if numero % 2 == 0:
-        numerosPares = numerosPares + numero
-        print(f"o número {numero} é par")
-
-print(f'Soma dos numeros pares: {numerosPares}')
